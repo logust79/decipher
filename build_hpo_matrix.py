7fee50952cb964e0d2474b3bc31dba0525f61660
logust79 <logust@yahoo.com>
2017-04-06 00:38:03 +0100

    finalised script

--- build_hpo_matrix.py
+++ build_hpo_matrix.py
@@ -11,12 +11,18 @@ convert csv to cooc matrix, main logic
 '''
 def csv_to_cooc(csv_file):
     # read csv
+    print('read csv')
     csv_df = pd.read_csv(csv_file)
     # get unique terms
+    print('get term set')
     term_set = get_term_set(csv_df)
     # build patient-hpo matrix
+    print('build patient-hpo matrix')
     df = build_patient_hpo_df(csv_df, term_set)
+    # fill nan
+    df = df.fillna(0)
     # co-oc matrix
+    print('build co-oc matrix')
     df = df.astype(int)
     return df.T.dot(df)
 
@@ -25,10 +31,12 @@ get unique terms
 '''
 def get_term_set(df):
     def union(a,b):
-        if isinstance(b,int):
+        if pd.isnull(b):
+            return a
+        if not isinstance(b,str):
             return None
         return ';'.join([a,b])
-    term_set = df.apply(lambda x: reduce(union,x), axis=0)['type']
+    term_set = df.apply(lambda x: reduce(union,x), axis=0)['phenotype']
     return set(term_set.split(';'))
 
 '''
@@ -37,7 +45,7 @@ build patient-hpo df
 def build_patient_hpo_df(df, term_set):
     result = pd.DataFrame()
     for t in term_set:
-        result[t] =df['type'].str.contains(t)
+        result[t] =df['phenotype'].str.contains(t)
     return result
 
 
@@ -51,4 +59,6 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
 
     result = csv_to_cooc(options.input)
-    print(result)
+    store = pd.HDFStore('output/store.h5')
+    store['hpo_cooc'] = result
+    print('done')
